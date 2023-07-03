4e2efa8f60584d698bbb3d032dda84ce8af96395
Abdul Omar <abbcc123123@gmail.com>
2023-07-03 19:27:20 -0400

    add acknowledge and agree

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -74,12 +74,12 @@ const Login = () => {
   };
 
   return (
-    <div className="w-full flex flex-col items-center justify-center gap-10 py-20">
-      <div className=" w-full flex items-center justify-center gap-10 ">
+    <div className="w-full flex items-center justify-center gap-10 py-20">
+      <div className="w-full flex flex-col items-center justify-center gap-10">
         {!currentUser && (
           <div
             onClick={handleGoogleLogin}
-            className=" text-base w-60 h-12 tracking-wide border-[1px] border-gray-400 rounded-md flex items-center justify-center gap-2 hover:border-blue-600 cursor-pointer duration-300 "
+            className="text-base w-60 h-12 tracking-wide border-[1px] border-gray-400 rounded-md flex items-center justify-center gap-2 hover:border-blue-600 cursor-pointer duration-300"
           >
             <img className="w-8" src={googlelogo} alt="" />
             <span className="text-sm">Sign in with google</span>
@@ -88,11 +88,19 @@ const Login = () => {
         {currentUser && (
           <button
             onClick={handleSignOut}
-            className=" bg-black text-white text-base py-3 px-8 tracking-wide rounded-md hover:bg-gray-800 duration-300 "
+            className="bg-black text-white text-base py-3 px-8 tracking-wide rounded-md hover:bg-gray-800 duration-300"
           >
             Sign Out
           </button>
         )}
+        <p className="text-sm text-gray-500 mt-4 ml-[10%] mr-[10%] ">
+          By signing up, you acknowledge and agree that the email address, name,
+          and profile picture associated with your Google account will be stored
+          in our database. This information will be used in accordance with our
+          Privacy Policy to enhance your user experience. Please ensure you have
+          reviewed and understand our Privacy Policy and Terms of Service before
+          proceeding.
+        </p>
       </div>
 
       <ToastContainer
