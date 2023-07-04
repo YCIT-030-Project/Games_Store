4d0f49720170e13075681aed5edcd4c908b4f05c
raabdi <96847829+ridwaangaas@users.noreply.github.com>
2023-07-04 04:09:52 -0400

    - added submit button - added the different functions of submission event handler - change the for into htmlFor to get rid of the errors shown on console

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
+const handleEmailChange = (e) => {
+  setEmail(e.target.value);
+};
+
+const handleSubjectChange = (e) => {
+  setSubject(e.target.value);
+};
+
+const handleMessageChange = (e) => {
+  setMessage(e.target.value);
+};
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  console.log('Email:', email);
+  console.log('Subject:', subject);
+  console.log('Message:', message);
+
+  setEmail('');
+  setSubject('');
+  setMessage('');
+};
+
   return (
     <section className="bg-white ">
       <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
@@ -11,10 +38,10 @@ const Contact = () => {
           Got a technical issue? Want to send feedback about a beta feature?
           Need details about our Business plan? Let us know.
         </p>
-        <form action="#" className="space-y-8">
+        <form onSubmit={handleSubmit} action="#" className="space-y-8">
           <div>
             <label
-              for="email"
+              htmlFor="email"
               className="block mb-2 text-sm font-medium text-gray-900 "
             >
               Your email
@@ -22,6 +49,8 @@ const Contact = () => {
             <input
               type="email"
               id="email"
+              value={email}
+              onChange={handleEmailChange}
               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
               placeholder="name@flowbite.com"
               required
@@ -29,7 +58,7 @@ const Contact = () => {
           </div>
           <div>
             <label
-              for="subject"
+              htmlFor="subject"
               className="block mb-2 text-sm font-medium text-gray-900 "
             >
               Subject
@@ -37,6 +66,8 @@ const Contact = () => {
             <input
               type="text"
               id="subject"
+              value={subject}
+              onChange={handleSubjectChange}
               className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 "
               placeholder="Let us know how we can help you"
               required
@@ -44,18 +75,23 @@ const Contact = () => {
           </div>
           <div className="sm:col-span-2">
             <label
-              for="message"
+              htmlFor="message"
               className="block mb-2 text-sm font-medium text-gray-900"
             >
               Your message
             </label>
             <textarea
               id="message"
+              value={message}
+              onChange={handleMessageChange}
               rows="6"
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 "
               placeholder="Leave a comment..."
             ></textarea>
           </div>
+          <button type="submit" className="px-4 py-2 bg-gray-100 text-gray-500 
+          rounded mx-auto block mt-4
+           hover:border hover:border-black ">Submit</button>
         </form>
       </div>
     </section>
